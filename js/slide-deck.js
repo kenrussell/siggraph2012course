ad65b503a5f2d30ca2e82978ca385373259285df
Kenneth Russell <kbr@chromium.org>
2012-07-31 17:11:02 -0700

    Beginning of WebGL presentation, copying slides, figures, and code from preexisting presentations. Added lang-glsl.js to prettify and loaded and used it. This is an intermediate commit to be able to reproduce some rendering issues on other machines.

--- js/slide-deck.js
+++ js/slide-deck.js
@@ -315,7 +315,7 @@ SlideDeck.prototype.loadConfig_ = function(config) {
   }
 
   if (settings.title) {
-    document.title = settings.title.replace(/<br\/?>/, ' ') + ' - Google IO 2012';
+    document.title = settings.title.replace(/<br\/?>/, ' ') + ' - SIGGRAPH 2012';
     document.querySelector('[data-config-title]').innerHTML = settings.title;
   }
 
